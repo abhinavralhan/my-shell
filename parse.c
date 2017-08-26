31f894db0715ab720b250e7c6ac01abc178bc1eb
abhinav ralhan <abhinav.ralhan1@gmail.com>
2017-08-27 00:04:55 +0530

    first

---
+++ parse.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+/**
+ *  parse.c
+ */
+
+
+/*  void init_info(parseInfo *p) {
+      initialize parseInfo struct
+  }
+*/
+  /* parse a single command */
+  /*void parse_command(char * command, struct commandType *comm) {
+
+  }
+*/
+  /*   parse commandline for space separated commands */
+  /*parseInfo *parse (char *cmdline) {
+    foreach cmd in cmdline {
+      if (cmd == command) {
+        parse_command(cmd, type)
+      }
+    }
+  }
+*/
+  /* prints out parse struct */
+  /*void print_info (parseInfo *info) {
+     foreach type in parseInfo {
+       print "type_name: type"
+     }
+  }  
+*/
+  /*  free memory used in parseInfo */
+/*  void free_info (parseInfo *info) {
+     foreach memory_block in parseInfo
+        free(memory_block)
+  }
+
+*/
+  int main( int argc, char *argv[] )  {
+
+/*   if( argc == 2 ) {
+      printf("The argument supplied is %s\n", argv[1]);
+   }
+   else if( argc > 2 ) {
+    
+        for (int i=1; i < argc; i++) {
+          printf("arg %d = %s\n", i, argv[i]);
+        }
+   }
+   else {
+      printf("One argument expected.\n");
+   }*/
+
+    
+
+    
+/*    while(1) {
+       cmdLine = readline(printPrompt());
+
+       info  = parse(cmdLine);
+
+       print_info(info);
+    }
+
+    free_info(info);*/
+ }
